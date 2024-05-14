77b1da4a33e7cabcff07c8fd670384b9358e2e77
Eskibekov Mirat <eskibekov14@gmail.com>
2024-05-14 12:56:41 +0500

    Last

--- src/main/java/com/example/stepik/repositories/PermissionRepository.java
+++ src/main/java/com/example/stepik/repositories/PermissionRepository.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
     Permission findAllByRole(String role);
 }
--- src/main/java/com/example/stepik/services/impl/TaskServiceImpl.java
+++ src/main/java/com/example/stepik/services/impl/TaskServiceImpl.java
@@ -1,15 +1,25 @@
 package com.example.stepik.services.impl;
 
+import com.example.stepik.auth.AuthenticationRequest;
+import com.example.stepik.auth.AuthenticationService;
 import com.example.stepik.entities.Status;
 import com.example.stepik.entities.Task;
+import com.example.stepik.entities.Users;
 import com.example.stepik.entitiesDTO.TaskDto;
+import com.example.stepik.jwt.JwtAuthenticationFilter;
+import com.example.stepik.jwt.JwtService;
 import com.example.stepik.mappers.TaskMapper;
 import com.example.stepik.mappers.UsersMapper;
 import com.example.stepik.repositories.StatusRepository;
 import com.example.stepik.repositories.TaskRepository;
+import com.example.stepik.repositories.UsersRepository;
 import com.example.stepik.services.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,9 +30,20 @@ public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
+    private final UsersRepository usersRepository;
+
+
 
     @Override
     public List<TaskDto> getTaskByUser(Long userId) {
+        Users user = usersRepository.findAllById(userId);
+        if(user!=null){
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String currentEmail = authentication.getName();
+            if(!user.getEmail().equals(currentEmail)){
+                return null;
+            }
+        }
         return taskMapper.mapToTaskDtoList(taskRepository.findAllByUser_Id(userId));
     }
 
